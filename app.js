4ef128f7eb2a80645ec32fc612ffff0a3e0702a0
cwh0204 <48270074+cwh0204@users.noreply.github.com>
2023-09-01 21:52:22 +0900

    const_list

---
+++ app.js
@@ -0,0 +1,11 @@
+const a = 10;
+const b = 3;
+const c = "Hellos";
+const daysWeek = ["mon","tue","wed","the","fri","set"];
+console.log(b*a);
+console.log(b+a);
+console.log(b-a);
+console.log(c + "cwh~");
+console.log(daysWeek[0])
+daysWeek.push("sun");
+console.log(daysWeek)
