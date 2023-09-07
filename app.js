f780c3f7b4a31efc4e06d065ee3765c6ba1c7c9c
cwh0204 <48270074+cwh0204@users.noreply.github.com>
2023-09-07 21:37:46 +0900

    Task 1
    Task 1

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
-const a = 10;
-const b = 3;
-const c = "Hellos";
-const daysWeek = ["mon","tue","wed","the","fri","set"];
-console.log(b*a);
-console.log(b+a);
-console.log(b-a);
-console.log(c + "cwh~");
-console.log(daysWeek[0])
-daysWeek.push("sun");
-console.log(daysWeek)
-//2-5
+const title = document.querySelector("h1");
+const colors = ["skyblue", "blue" , "purple" , "tomato"];
+const superEventHandler = {
+    handleMouseEnter: function (){
+        title.innerText = "mouse in here!"
+        title.style.color=colors[0];
+    },
+    handleMouseLeave: function (){
+        title.innerText = "mouse in gond!"
+        title.style.color=colors[1];
+    },
+    handleWindowResize: function (){
+        title.innerText = "You just resize!"
+        title.style.color=colors[2];
+    },
+    handleTitleClick: function (){
+        title.innerText = "That was a right click!"
+        title.style.color=colors[3];
+    }
+};
+
+title.addEventListener("mouseenter", superEventHandler.handleMouseEnter);
+title.addEventListener("mouseleave", superEventHandler.handleMouseLeave);
+window.addEventListener("resize", superEventHandler.handleWindowResize);
+window.addEventListener("contextmenu", superEventHandler.handleTitleClick);
