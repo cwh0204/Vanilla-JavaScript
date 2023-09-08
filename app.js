380ace4cb75a4284b13e978245773edd847c0b40
cwh0204 <48270074+cwh0204@users.noreply.github.com>
2023-09-08 14:03:40 +0900

    index 2

--- app.js
+++ app.js
@@ -1,25 +1,20 @@
-const title = document.querySelector("h1");
-const colors = ["skyblue", "blue" , "purple" , "tomato"];
-const superEventHandler = {
-    handleMouseEnter: function (){
-        title.innerText = "mouse in here!"
-        title.style.color=colors[0];
-    },
-    handleMouseLeave: function (){
-        title.innerText = "mouse in gond!"
-        title.style.color=colors[1];
-    },
-    handleWindowResize: function (){
-        title.innerText = "You just resize!"
-        title.style.color=colors[2];
-    },
-    handleTitleClick: function (){
-        title.innerText = "That was a right click!"
-        title.style.color=colors[3];
+const title = document.querySelector("body");
+const title2 = document.querySelector("h2")
+const colors = ["yellow","purple","skyblue","tomato"];
+title2.style.color = "white"
+title.style.backgroundColor = colors[1];
+function handleResize(){
+    var wsize = window.innerWidth;
+    console.log(wsize);
+    if(wsize <= 400){
+        title.style.backgroundColor = colors[2];
     }
-};
+    else if(wsize > 400 && wsize <= 800 ){
+        title.style.backgroundColor = colors[1];
+    }
+    else if(wsize > 800 && wsize <= 1200 ){
+        title.style.backgroundColor = colors[0];
+    }
+}
 
-title.addEventListener("mouseenter", superEventHandler.handleMouseEnter);
-title.addEventListener("mouseleave", superEventHandler.handleMouseLeave);
-window.addEventListener("resize", superEventHandler.handleWindowResize);
-window.addEventListener("contextmenu", superEventHandler.handleTitleClick);
+window.addEventListener("resize",handleResize);
